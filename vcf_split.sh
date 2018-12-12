11d0226b4a519d8b836323e7c2c8752fa87b4aee
mankek <krystal.manke@gmail.com>
2018-12-12 16:57:50 -0600

    added c option to list variant callers; not final purpose

--- vcf_split.sh
+++ vcf_split.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 
 usage() {
-	echo "Usage: $0 [-h] [-i] [p] [s]"
+	echo "Usage: $0 [-h] [c] [-i] [p] [s]"
 	echo "	-h	Help."
 	echo "	-i	Set multi-sample VCF"
+	echo "  -c      Specify variant caller"
 	echo "	-p	Print patient names in input file"
 	echo "	-s	Perform splitting of input vcf"
 	echo "This is a help message"
 	exit
 }
 
-while getopts "hi:sp" optchar;
+while getopts "hi:c:sp" optchar;
 do
 	case $optchar in
 		i)
 			file="$OPTARG"
 			;;
+		c)
+			caller="$OPTARG"
+			;;
 		s)
-			for f in $(awk '/^#CHROM/ {for (i=10; i<=NF; i++) print $i; }' $file)
-                        do
-                                vcftools --vcf $file --indv $f --recode --out $f
-                        done
+			if test -z "$file"
+			then
+				echo "No input file!"
+				usage
+			else
+				for f in $(awk '/^#CHROM/ {for (i=10; i<=NF; i++) print $i; }' $file)
+                        	do
+                                	vcftools --vcf $file --indv $f --recode --out $f
+                        	done
+			fi
                         ;;
 		p)
-			for f in $(awk '/^#CHROM/ {for (i=10; i<=NF; i++) print $i; }' $file)
-                        do
-                                echo "$f"
-                        done
+			if test -z "$file"
+			then
+				echo "No input file!"
+				usage
+			else
+				for f in $(awk '/^#CHROM/ {for (i=10; i<=NF; i++) print $i; }' $file)
+                        	do
+					if test -z "$caller"
+					then
+                                		echo "$f"
+					else
+						echo "$f"|awk -F'.' '{ print $2 }'
+					fi
+                        	done
+			fi
                         ;;
 		\?)
 			echo "Invalid option: -$OPTARG"
