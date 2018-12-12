78290866a0578d95f3a4897c8bd316716f48ba39
mankek <krystal.manke@gmail.com>
2018-12-12 14:57:38 -0600

    Added patient display option and help

--- README.md
+++ README.md
@@ -2,4 +2,12 @@ This repository is for a small, simple command-line tool that takes a multi-samp
 VCFs. Requires that vcftools be installed and if there are multiple variant callers used on the same samples within the 
 multi-sample file, a different vcf will be made for each variant caller used.
 
-Work in Progress
+Use:
+
+-h: Displays help menu
+
+-i: Specifies input multi-sample vcf
+
+-p: Prints a list of all patients found in the input file
+
+-s: performs splitting on the input file
--- vcf_split.sh
+++ vcf_split.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+usage() {
+	echo "Usage: $0 [-h] [-i] [p] [s]"
+	echo "	-h	Help."
+	echo "	-i	Set multi-sample VCF"
+	echo "	-p	Print patient names in input file"
+	echo "	-s	Perform splitting of input vcf"
+	echo "This is a help message"
+	exit
+}
 
-while getopts ":i:" optchar;
+while getopts "hi:sp" optchar;
 do
 	case $optchar in
 		i)
-			for f in $(awk '/^#CHROM/ {for (i=10; i<=NF; i++) print $i; }' $OPTARG)
-			do
-				vcftools --vcf $OPTARG --indv $f --recode --out $f
-			done
+			file="$OPTARG"
 			;;
+		s)
+			for f in $(awk '/^#CHROM/ {for (i=10; i<=NF; i++) print $i; }' $file)
+                        do
+                                vcftools --vcf $file --indv $f --recode --out $f
+                        done
+                        ;;
+		p)
+			for f in $(awk '/^#CHROM/ {for (i=10; i<=NF; i++) print $i; }' $file)
+                        do
+                                echo "$f"
+                        done
+                        ;;
 		\?)
 			echo "Invalid option: -$OPTARG"
 			exit 1
@@ -17,6 +35,9 @@ do
 		:)
 			echo "Option -$OPTARG requires an argument."
 			exit 1
+			;;
+		h)
+			usage
 			;;
 	esac
 done
